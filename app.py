7036324ab3da74682974d50a4dd4ebe4958b0079
esnamans <116254258+esnamans@users.noreply.github.com>
2023-05-05 18:31:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -138,23 +138,7 @@ def main():
         
     st.sidebar.subheader("Prediksi Kanker Payudara")
     
-    if st.sidebar.checkbox("Hasil baru", False, key='lihat80'):
-        st.subheader("Random Forest")
-        dfnew = pd.read_csv('coimbra dataset breast sudah rfe.csv')
-        label = LabelEncoder()
-        for col in data.columns:
-        data[col] = label.fit_transform(data[col])
-        y2 = dfnew.Classification
-        x2 = dfnew.drop(columns =['Classification'])
-        x_train2, x_test2, y_train2, y_test2 = train_test_split(x2, y2, test_size=0.3, random_state=1)
-        model2 = RandomForestClassifier(n_estimators=100, max_depth=1,random_state=3)
-        model2.fit(x_train2, y_train2)
-        Age = st.number_input("Masukan Umur") 
-        BMI = st.number_input("Masukan BMI") 
-        Restitin = st.number_input("Restitin") 
-        Glucose = st.number_input("Glucose")
-        hasil_1 = model2.predict(Age, BMI, Restitin, Glucose)
-        st.write(hasil_1)
+ 
     
 
 if __name__ == '__main__':
